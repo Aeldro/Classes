db50ca205aa7b1f4f7bdf453e597d521cb0edae4
Nolan DE PUYDT <nodepuydt@gmail.com>
2024-06-17 09:44:56 +0200

    Ajoutez des fichiers projet.

---
+++ Classes/Character.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Character
+    {
+        public string _name;
+        public int _life;
+        public int _attack;
+        public int _defense;
+
+        public Character(string name, int life, int attack, int defense)
+        {
+            this._name = name;
+            this._life = life;
+            this._attack = attack;
+            this._defense = defense;
+        }
+
+
+        public bool IsAlive()
+        {
+            if (this._life > 0) return true;
+            else return false;
+        }
+
+        public void Attack(Character character)
+        {
+            character._life = character._life - (character._attack - character._defense);
+        }
+    }
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Character firstCharacter = new Character("Paul", 120, 10, 50);
+            Character secondCharacter = new Character("Roro", 70, 60, 20);
+
+            while (firstCharacter.IsAlive() && secondCharacter.IsAlive())
+            {
+                firstCharacter.Attack(secondCharacter);
+                secondCharacter.Attack(firstCharacter);
+                if (!firstCharacter.IsAlive())
+                {
+                    Console.WriteLine($"{secondCharacter._name} a mis une branlée à {firstCharacter._name}. Cheh à lui.");
+                }
+                else if (secondCharacter.IsAlive())
+                {
+                    Console.WriteLine($"{firstCharacter._name} a mis une branlée à {secondCharacter._name}. Cheh à lui.");
+
+                }
+            }
+        }
+    }
+}
